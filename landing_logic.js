a6afebee7ab84c5f4325e878d48cf7bd99402682
system-out-cho <yuchan722cho@gmail.com>
2026-01-26 18:09:13 -0500

    added background

--- landing_logic.js
+++ landing_logic.js
@@ -3,22 +3,33 @@ window.onload = function () {
   const buttonRow = document.querySelector(".button-row");
   const checkOutText = document.querySelector("#check-out");
   const barcode = document.querySelector("#barcode");
+  const hoverBox2 = document.querySelector("#hover-box2");
+  const col3 = document.querySelector("#column3");
 
-  AnimateText(barcode, "MYPORTFOLIO");
+  AnimateText(barcode, "MYPORTFOLIOYC");
   AnimateText(checkOutText, "CHECK OUT");
 
   buttonRow.addEventListener("mousemove", (e) => {
     hoverBox.style.background = "white";
+    hoverBox2.style.background = "black";
     const bounding = buttonRow.getBoundingClientRect();
+    const bounding2 = col3.getBoundingClientRect();
 
     const x = e.clientX - bounding.left;
-    const y = e.clientY - bounding.top;
+    const x2 = e.clientX - bounding2.left;
 
     hoverBox.style.transform = `translate(${x - hoverBox.offsetWidth / 2}px`;
+
+    hoverBox2.style.transform = `translate(${0}px, ${x / 3 - hoverBox2.offsetHeight / 2}px)`;
   });
 
   buttonRow.addEventListener("mouseleave", (e) => {
     hoverBox.style.background = "transparent";
+    hoverBox2.style.background = "transparent";
+  });
+
+  button1.addEventListener("mousemove", (e) => {
+    console.log("hovering");
   });
 };
 
