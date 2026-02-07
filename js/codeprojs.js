ac9ecc7fb9c5e944707c9b7590e1cc35e7028104
Yuchan Cho <cho@Yuchans-MacBook-Air.local>
2026-02-07 17:08:07 -0500

    added some new files

---
+++ js/codeprojs.js
@@ -0,0 +1,57 @@
+const path = '/json/CodingProjects.json'
+// const pagePath = '/pages/code.html'
+
+
+window.onload = function () {
+    //add hover for nav
+    AddHoverToNav("black", "transparent");
+    //making new swiper 
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 1,
+        spaceBetween: 10,
+
+        breakpoints: {
+        640: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 30,
+        },
+        },
+        
+        navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+        },
+
+        pagination: {
+        el: ".swiper-pagination-custom",
+        clickable: true,
+        },
+    });
+
+    fetchJsonData()
+    .then(data => setPageInformation(data));
+};
+
+
+async function fetchJsonData() {
+  try {
+    const response = await fetch(path);
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error('Error fetching JSON:', error);
+  }
+}
+
+function setPageInformation(data) {
+    const swiper_wrapper = document.querySelector(".swiper-wrapper");
+    console.log(data);
+    console.log(swiper_wrapper);
+}
