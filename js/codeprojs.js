a9eec27f45e9b9ae4e11c266bcc57a5df00044dd
system-out-cho <yuchan722cho@gmail.com>
2026-02-08 16:01:56 -0500

    fixing all links

--- js/codeprojs.js
+++ js/codeprojs.js
@@ -1,41 +1,39 @@
-const pagePath = '/pages/project_pages/';
-const imagePath = '/content/';
-
+const pagePath = "./pages/project_pages/";
+const imagePath = "./content/";
 
 window.onload = function () {
-    //add hover for nav
-    //making new swiper 
-    var swiper = new Swiper(".mySwiper", {
-        slidesPerView: 1,
-        spaceBetween: 10,
+  //add hover for nav
+  //making new swiper
+  var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 10,
 
-        breakpoints: {
-        640: {
-            slidesPerView: 2,
-            spaceBetween: 20,
-        },
-        1024: {
-            slidesPerView: 4,
-            spaceBetween: 30,
-        },
-        },
-        
-        navigation: {
-        nextEl: ".swiper-button-next",
-        prevEl: ".swiper-button-prev",
-        },
+    breakpoints: {
+      640: {
+        slidesPerView: 2,
+        spaceBetween: 20,
+      },
+      1024: {
+        slidesPerView: 4,
+        spaceBetween: 30,
+      },
+    },
 
-        pagination: {
-        el: ".swiper-pagination-custom",
-        clickable: true,
-        },
-    });
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
 
-    // fetchJsonData()
-    // .then(data => setPageInformation(data));
-    setPageInformation();
+    pagination: {
+      el: ".swiper-pagination-custom",
+      clickable: true,
+    },
+  });
+
+  // fetchJsonData()
+  // .then(data => setPageInformation(data));
+  setPageInformation();
 };
-
 
 // async function fetchJsonData() {
 //   try {
@@ -51,11 +49,11 @@ window.onload = function () {
 // }
 
 function setPageInformation() {
-    const swiper_wrapper = document.querySelector(".swiper-wrapper");
-    
-    for (let i = 0; i < data.length; i++) {
-        const proj = data[i];
-        const slideTemplate = `
+  const swiper_wrapper = document.querySelector(".swiper-wrapper");
+
+  for (let i = 0; i < data.length; i++) {
+    const proj = data[i];
+    const slideTemplate = `
             <div class="swiper-slide">
                 <div class="img-wrapper">
                     <a href="${pagePath + proj.link}" class="project-link">
@@ -64,8 +62,7 @@ function setPageInformation() {
                     </a>
                 </div>
             </div>`;
-        
-        swiper_wrapper.insertAdjacentHTML('beforeend', slideTemplate);
-    }
 
+    swiper_wrapper.insertAdjacentHTML("beforeend", slideTemplate);
+  }
 }
