4d9e2318ff213598a3aaf56a845332e295dc2385
system-out-cho <yuchan722cho@gmail.com>
2026-02-09 12:18:40 -0500

    making it easier to add 3D work projects

---
+++ js/artprojs.js
@@ -0,0 +1,79 @@
+const imagePath = "../content/";
+
+window.onload = function () {
+  console.log("testing");
+  setPageInformation();
+};
+
+function setPageInformation() {
+  let page_body = document.querySelector(".page-body");
+
+  for (let i = 0; i < data.length; i++) {
+    const data_obj = data[i];
+    const projHTML = makeProjHTML(data_obj);
+    page_body.insertAdjacentHTML("afterbegin", projHTML);
+  }
+}
+
+function makeProjHTML(data_obj) {
+  let media;
+  let summaryTemplate;
+  const type = data_obj.media_type;
+  const media_link = data_obj.media_link;
+  const summary_exists = data_obj.summary_exists;
+
+  if (type === "video") {
+    media = `<iframe
+            src="${media_link}"
+            title="YouTube video player"
+            frameborder="0"
+            allow="
+                accelerometer;
+                autoplay;
+                clipboard-write;
+                encrypted-media;
+                gyroscope;
+                picture-in-picture;
+                web-share;
+            "
+            referrerpolicy="strict-origin-when-cross-origin"
+            allowfullscreen
+            ></iframe>`;
+  } else if (type === "image") {
+    media = `<img src="${imagePath + media_link}" alt="test-image" />`;
+  } else {
+    console.log("incorrect media type");
+  }
+
+  if (summary_exists) {
+    summaryTemplate = `        
+        <details class="learn-more">
+            <summary>LEARN MORE</summary>
+            <div class="expanded-content">
+                <p>Extra content here Lorem Ipsum asjdklajdasjdasjd.</p>
+            </div>
+        </details>
+    `;
+  } else {
+    summaryTemplate = ``;
+  }
+
+  const projTemplate = `
+    <div class="project">
+        <div class="project-column-1">
+        <div class="project-name">${data_obj.project_name}</div>
+        <div class="description">
+            ${data_obj.desc}
+        </div>
+            ${summaryTemplate}
+        </div>
+
+        <div class="project-column-2">
+        <div class="media-container">
+            ${media}
+        </div>
+        </div>
+    </div>`;
+
+  return projTemplate;
+}
---
+++ project_data/art_projects_data.js
@@ -0,0 +1,10 @@
+const data = [
+  {
+    project_name: "3D REEL",
+    desc: "A compilation of my past creative works, including personal and internship projects.",
+    summary_exists: false,
+    summary_link: "",
+    media_type: "video",
+    media_link: "https://www.youtube.com/embed/A_caxCQkZlc?si=JTEafP2pK5UKZHJJ",
+  },
+];
