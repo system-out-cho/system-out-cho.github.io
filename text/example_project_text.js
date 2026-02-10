282e0ed063d8be3e7504c6f7b39925750970ba3a
Yuchan Cho <cho@Yuchans-MacBook-Air.local>
2026-02-10 15:44:17 -0500

    trying new template

---
+++ text/example_project_text.js
@@ -0,0 +1,46 @@
+const project_content = [
+    {
+        title: "",
+        lang_tags: "",
+        software_tags: "",
+        desc_1: `Lorem ipsum dolor sit amet consectetur adipiscing elit. Ex
+                sapien vitae pellentesque sem placerat in id. Pretium tellus
+                duis convallis tempus leo eu aenean. Urna tempor pulvinar
+                vivamus fringilla lacus nec metus. Iaculis massa nisl malesuada
+                lacinia integer nunc posuere. Semper vel class aptent taciti
+                sociosqu ad litora. Conubia nostra inceptos himenaeos orci
+                varius natoque penatibus.`,
+        image_1: "",
+        image_2: "",
+        image_3: "",
+        desc_2: `pellentesque sem placerat in id cursus mi pretium tellus duis
+                convallis tempus leo eu aenean sed diam urna tempor pulvinar
+                vivamus fringilla lacus nec metus bibendum egestas iaculis massa
+                nisl malesuada lacinia integer nunc posuere ut hendrerit semper
+                vel class aptent taciti sociosqu ad litora torquent per conubia
+                nostra inceptos himenaeos orci varius natoque penatibus et magnis
+                dis parturient montes nascetur ridiculus mus donec rhoncus eros
+                lobortis nulla molestie mattis scelerisque maximus eget fermentum
+                odio phasellus non purus est efficitur laoreet mauris pharetra
+                vestibulum fusce`
+    },
+    {
+        title: "",
+        lang_tags: "",
+        software_tags: "",
+        desc_1: `Lorem ipsum dolor sit amet consectetur adipiscing elit. Ex
+                sapien vitae pellentesque sem placerat in id. Pretium tellus
+                duis convallis tempus leo eu aenean. Urna tempor pulvinar
+                vivamus fringilla lacus nec metus. Iaculis massa nisl malesuada
+                lacinia integer nunc posuere. Semper vel class aptent taciti
+                sociosqu ad litora. Conubia nostra inceptos himenaeos orci
+                varius natoque penatibus.`,
+        image_1: "",
+        image_2: "",
+        image_3: "",
+        desc_2: ""
+    },
+]
+
+// make empty html file, add all the html code via js (make another js script)
+// take content from this file 
