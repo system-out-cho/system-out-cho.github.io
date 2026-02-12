7353de1ec7234497fc91319d11b0db2a0bcd9a51
system-out-cho <yuchan722cho@gmail.com>
2026-02-12 15:03:40 -0500

    some new organization

--- text/example_project_text.js
+++ text/example_project_text.js
@@ -1,19 +1,65 @@
-const project_content = [
+// const project_content = [
+//   {
+//     title: "",
+//     lang_tags: "",
+//     software_tags: "",
+//     desc_1: `Lorem ipsum dolor sit amet consectetur adipiscing elit. Ex
+//                 sapien vitae pellentesque sem placerat in id. Pretium tellus
+//                 duis convallis tempus leo eu aenean. Urna tempor pulvinar
+//                 vivamus fringilla lacus nec metus. Iaculis massa nisl malesuada
+//                 lacinia integer nunc posuere. Semper vel class aptent taciti
+//                 sociosqu ad litora. Conubia nostra inceptos himenaeos orci
+//                 varius natoque penatibus.`,
+//     image_1: "",
+//     image_2: "",
+//     image_3: "",
+//     desc_2: `pellentesque sem placerat in id cursus mi pretium tellus duis
+//                 convallis tempus leo eu aenean sed diam urna tempor pulvinar
+//                 vivamus fringilla lacus nec metus bibendum egestas iaculis massa
+//                 nisl malesuada lacinia integer nunc posuere ut hendrerit semper
+//                 vel class aptent taciti sociosqu ad litora torquent per conubia
+//                 nostra inceptos himenaeos orci varius natoque penatibus et magnis
+//                 dis parturient montes nascetur ridiculus mus donec rhoncus eros
+//                 lobortis nulla molestie mattis scelerisque maximus eget fermentum
+//                 odio phasellus non purus est efficitur laoreet mauris pharetra
+//                 vestibulum fusce`,
+//   },
+//   {
+//     title: "",
+//     lang_tags: "",
+//     software_tags: "",
+//     desc_1: `Lorem ipsum dolor sit amet consectetur adipiscing elit. Ex
+//                 sapien vitae pellentesque sem placerat in id. Pretium tellus
+//                 duis convallis tempus leo eu aenean. Urna tempor pulvinar
+//                 vivamus fringilla lacus nec metus. Iaculis massa nisl malesuada
+//                 lacinia integer nunc posuere. Semper vel class aptent taciti
+//                 sociosqu ad litora. Conubia nostra inceptos himenaeos orci
+//                 varius natoque penatibus.`,
+//     image_1: "",
+//     image_2: "",
+//     image_3: "",
+//     desc_2: "",
+//   },
+// ];
+
+const project_content = new Map([
+  [
+    1,
     {
-        title: "",
-        lang_tags: "",
-        software_tags: "",
-        desc_1: `Lorem ipsum dolor sit amet consectetur adipiscing elit. Ex
+      title: "test project id: 1",
+      lang_tags: ["Python", "Java"],
+      software_tags: ["Blender", "Something"],
+      desc_1: `Lorem ipsum dolor sit amet consectetur adipiscing elit. Ex
                 sapien vitae pellentesque sem placerat in id. Pretium tellus
                 duis convallis tempus leo eu aenean. Urna tempor pulvinar
                 vivamus fringilla lacus nec metus. Iaculis massa nisl malesuada
                 lacinia integer nunc posuere. Semper vel class aptent taciti
                 sociosqu ad litora. Conubia nostra inceptos himenaeos orci
                 varius natoque penatibus.`,
-        image_1: "",
-        image_2: "",
-        image_3: "",
-        desc_2: `pellentesque sem placerat in id cursus mi pretium tellus duis
+      image_1: "",
+      image_2: "",
+      image_3: "",
+      desc_2: `pellentesque sem placerat in id cursus mi pretium tellus duis
                 convallis tempus leo eu aenean sed diam urna tempor pulvinar
                 vivamus fringilla lacus nec metus bibendum egestas iaculis massa
                 nisl malesuada lacinia integer nunc posuere ut hendrerit semper
@@ -22,25 +68,38 @@ const project_content = [
                 dis parturient montes nascetur ridiculus mus donec rhoncus eros
                 lobortis nulla molestie mattis scelerisque maximus eget fermentum
                 odio phasellus non purus est efficitur laoreet mauris pharetra
-                vestibulum fusce`
+                vestibulum fusce`,
     },
+  ],
+  [
+    2,
     {
-        title: "",
-        lang_tags: "",
-        software_tags: "",
-        desc_1: `Lorem ipsum dolor sit amet consectetur adipiscing elit. Ex
+      title: "test project id: 2",
+      lang_tags: ["lang1", "lang2"],
+      software_tags: ["software1", "software2"],
+      desc_1: `Lorem ipsum dolor sit amet consectetur adipiscing elit. Ex
                 sapien vitae pellentesque sem placerat in id. Pretium tellus
                 duis convallis tempus leo eu aenean. Urna tempor pulvinar
                 vivamus fringilla lacus nec metus. Iaculis massa nisl malesuada
                 lacinia integer nunc posuere. Semper vel class aptent taciti
                 sociosqu ad litora. Conubia nostra inceptos himenaeos orci
                 varius natoque penatibus.`,
-        image_1: "",
-        image_2: "",
-        image_3: "",
-        desc_2: ""
+      image_1: "",
+      image_2: "",
+      image_3: "",
+      desc_2: `pellentesque sem placerat in id cursus mi pretium tellus duis
+                convallis tempus leo eu aenean sed diam urna tempor pulvinar
+                vivamus fringilla lacus nec metus bibendum egestas iaculis massa
+                nisl malesuada lacinia integer nunc posuere ut hendrerit semper
+                vel class aptent taciti sociosqu ad litora torquent per conubia
+                nostra inceptos himenaeos orci varius natoque penatibus et magnis
+                dis parturient montes nascetur ridiculus mus donec rhoncus eros
+                lobortis nulla molestie mattis scelerisque maximus eget fermentum
+                odio phasellus non purus est efficitur laoreet mauris pharetra
+                vestibulum fusce`,
     },
-]
+  ],
+]);
 
 // make empty html file, add all the html code via js (make another js script)
-// take content from this file 
+// take content from this file
