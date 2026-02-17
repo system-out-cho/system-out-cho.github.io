f2f9986505f00cc32e90ec1db6f02e874bfada5d
Yuchan Cho <cho@Mac.lan>
2026-02-16 17:02:45 -0800

    finished first final version

--- project_data/coding_projects_data.js
+++ project_data/coding_projects_data.js
@@ -12,6 +12,12 @@ const data = [
     alt: "An image of a ComfyUI node",
   },
   {
+    link: "personalportfolio.html",
+    title: "Custom Portfolio Website",
+    image: "figma_screenshot.png",
+    alt: "A screenshot of figma with this website's layouts.",
+  },
+  {
     link: "poserecognition.html",
     title: "AI Pose Recognition",
     image: "pose_recognition.png",
@@ -22,11 +28,5 @@ const data = [
     title: "Spotify Wrapped Clone",
     image: "spotify_wrapped.png",
     alt: "A demo image of the application.",
-  },
-  {
-    link: "personalportfolio.html",
-    title: "Custom Portfolio Website",
-    image: "figma_screenshot.png",
-    alt: "A screenshot of figma with this website's layouts.",
   },
 ];
